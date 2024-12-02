04a163869c4c9ca99c90e0c8bcf44d6a614916a7
Glen Nelson <glen.nelson.1@gmail.com>
2024-12-01 22:44:19 -0700

    Implementation for part2

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -104,28 +104,56 @@ pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
     return filtered_levels.len() as u32;
 }
 
-/// Foo
-/// ```
-/// let vec1: Vec<String> = vec![
-///     "foo"
-/// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day02::puzzle_b(&vec1), 0);
-/// ```
-pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
-    return 0;
+/// https://en.wikipedia.org/wiki/Power_set
+fn power_set<T: Clone>(a_vector: &Vec<T>) -> Vec<Vec<T>> {
+    return a_vector
+        .iter()
+        // Fold - initial value, |curValue, next value|
+        .fold(vec![vec![]], |mut power_set, value| {
+            // Clone the previous sets, add the value to every set, thats the new sets
+            let new_sets = power_set.clone().into_iter().map(|mut previous_set| {
+                previous_set.push(value.clone());
+                return previous_set;
+            });
+            power_set.extend(new_sets);
+            return power_set;
+        });
 }
 
-/// Delete this after starting on puzzle_a.
-/// ```
-/// let vec1: Vec<u32> = vec![];
-/// let vec2: Vec<u32> = vec![1];
-/// assert_eq!(day02::coverage_workaround(&vec1), 1);
-/// assert_eq!(day02::coverage_workaround(&vec2), 2);
-/// ```
-pub fn coverage_workaround(a: &Vec<u32>) -> u32 {
-    if a.len() == 0 {
-        return 1;
-    } else {
-        return 2;
+fn is_safe_dampener(report: &Vec<u32>, dampener: usize) -> bool {
+    if is_safe(report) {
+        return true;
     }
+    let min_size = report.len() - dampener;
+    // generate the powersets, cull those too small, return immediately if we have found one that works
+    for possible_report in power_set(report) {
+        if possible_report.len() < min_size {
+            continue;
+        }
+        if is_safe(&possible_report) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/// Find at most 1 unsafe level
+/// ```
+/// let vec1: Vec<String> = vec![
+///     "7 6 4 2 1",
+///     "1 2 7 8 9",
+///     "9 7 6 2 1",
+///     "1 3 2 4 5",
+///     "8 6 4 4 1",
+///     "1 3 6 7 9"
+/// ].iter().map(|s| s.to_string()).collect();
+/// assert_eq!(day02::puzzle_b(&vec1), 4);
+/// ```
+pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
+    let levels = parse(string_list);
+    let filtered_levels: Vec<Vec<u32>> = levels
+        .into_iter()
+        .filter(|report| is_safe_dampener(report, 1))
+        .collect();
+    return filtered_levels.len() as u32;
 }
