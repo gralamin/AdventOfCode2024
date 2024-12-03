9dc4f63a0d70c8e012ab1e8762511271f9ea441f
Glen Nelson <glen.nelson.1@gmail.com>
2024-12-02 22:42:54 -0700

    Day03: simple parse and filter. Probably a cleaner way somehow but this parsing works.

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -33,13 +33,53 @@ pub fn puzzle_a(string_list: &Vec<String>) -> i32 {
         .sum();
 }
 
-/// Foo
+/// Split by dos and don't instructions, throw away the don'ts.
+fn split_by_dos_and_donts(string_list: &Vec<String>) -> Vec<String> {
+    let mut result = vec![];
+    let mut in_do = true;
+    let mut last_in_do = false;
+    for s in string_list {
+        let parts = s.split("don't()");
+        for p in parts {
+            last_in_do = false;
+            if in_do {
+                result.push(p.to_string());
+                in_do = false;
+                continue;
+            }
+            // Now definitely in a don't, split by do if any
+            let mut first_dont = true;
+            let do_parts = p.split("do()");
+            for j in do_parts {
+                if first_dont {
+                    first_dont = false;
+                    continue;
+                }
+                // these are all valid dos, push them in
+                result.push(j.to_string());
+                last_in_do = true;
+            }
+            // we are back in a don't definitely.
+        }
+        if last_in_do {
+            last_in_do = false;
+            in_do = true;
+        }
+    }
+    return result;
+}
+
+/// As 1, Work with dos and don't. We start in do()
 /// ```
 /// let vec1: Vec<String> = vec![
-///     "foo"
+///     "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 /// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day03::puzzle_b(&vec1), 0);
+/// assert_eq!(day03::puzzle_b(&vec1), 48);
 /// ```
 pub fn puzzle_b(string_list: &Vec<String>) -> i32 {
-    return 0;
+    let do_parts = split_by_dos_and_donts(string_list);
+    return extract_whole_muls(&do_parts)
+        .into_iter()
+        .map(|(x, y)| x * y)
+        .sum();
 }
