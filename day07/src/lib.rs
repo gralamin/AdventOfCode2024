40daeda1db1539b89dd75cd541725431c7239f4a
Glen Nelson <glen.nelson.1@gmail.com>
2024-12-06 22:37:27 -0700

    day07 solution to part A

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -3,15 +3,70 @@ extern crate filelib;
 pub use filelib::load_no_blanks;
 use log::info;
 
-/// Foo
+type Number = i64;
+
+fn parse_equations(input: &Vec<String>) -> Vec<(Number, Vec<Number>)> {
+    let mut result = vec![];
+    for line in input.into_iter() {
+        let (test, others) = line.split_once(":").unwrap();
+        let parsed_test = test.parse().unwrap();
+        let mut parsed_valued: Vec<Number> = vec![];
+        for v in others.trim().split(" ") {
+            let z = v.trim().parse().unwrap();
+            parsed_valued.push(z);
+        }
+
+        result.push((parsed_test, parsed_valued));
+    }
+    return result;
+}
+
+fn line_is_solvable(test_value: Number, numbers: &Vec<Number>) -> bool {
+    if numbers.len() <= 1 {
+        return test_value == numbers[0];
+    }
+    info!("Solving: {:?}", numbers);
+    let other_values = numbers[1..].to_vec();
+    return recurse_solve(test_value, &other_values, numbers[0]);
+}
+
+fn recurse_solve(target: Number, numbers: &Vec<Number>, current: Number) -> bool {
+    if current > target {
+        return false;
+    }
+    if numbers.is_empty() {
+        if current == target {
+            info!("Solved!");
+        }
+        return current == target;
+    }
+    let v = numbers[0];
+    let rest = numbers[1..].to_vec();
+    return recurse_solve(target, &rest, current + v) || recurse_solve(target, &rest, current * v);
+}
+
+/// Evaluate left ot right, and only + or *. Filter out unsolvable, sum the test values of others.
 /// ```
 /// let vec1: Vec<String> = vec![
-///     "foo"
+///     "190: 10 19",
+///     "3267: 81 40 27",
+///     "83: 17 5",
+///     "156: 15 6",
+///     "7290: 6 8 6 15",
+///     "161011: 16 10 13",
+///     "192: 17 8 14",
+///     "21037: 9 7 18 13",
+///     "292: 11 6 16 20"
 /// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day07::puzzle_a(&vec1), 0);
+/// assert_eq!(day07::puzzle_a(&vec1), 3749);
 /// ```
-pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
-    return 0;
+pub fn puzzle_a(string_list: &Vec<String>) -> Number {
+    let lines = parse_equations(string_list);
+    return lines
+        .into_iter()
+        .filter(|(test, others)| line_is_solvable(*test, others))
+        .map(|(test, _)| test)
+        .sum();
 }
 
 /// Foo
@@ -23,20 +78,4 @@ pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
 /// ```
 pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
     return 0;
-}
-
-/// Delete this after starting on puzzle_a.
-/// ```
-/// let vec1: Vec<u32> = vec![];
-/// let vec2: Vec<u32> = vec![1];
-/// assert_eq!(day07::coverage_workaround(&vec1), 1);
-/// assert_eq!(day07::coverage_workaround(&vec2), 2);
-/// ```
-pub fn coverage_workaround(a: &Vec<u32>) -> u32 {
-    if a.len() == 0 {
-        info!("Example logging of {:?}", a);
-        return 1;
-    } else {
-        return 2;
-    }
 }
